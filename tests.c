7e4fec95ffa20b58c0f0047ddf55c5b3889fc4bd
Piera Falcone <pierafalcone01@gmail.com>
2018-12-10 17:26:53 +0100

    added some tests

--- tests.c
+++ tests.c
@@ -224,9 +224,64 @@ TEST(tetramino_group_move_down)
 // Tests to make:
 
 // TEST(tetramino_group_move_left)
+TEST(tetramino_group_move_left)
+{
+	TETRAMINO_BOX_GROUP_SETUP(3, 3);
+	tetramino_group_move_down(tetramino_group, &tetris_map);
+	tetramino_group_move_right(tetramino_group, &tetris_map);
+	tetramino_group_move_left(tetramino_group, &tetris_map);
+
+	ASSERT_THAT(tetramino_group[0].x == 0);
+	ASSERT_THAT(tetramino_group[0].y == 1);
+
+	ASSERT_THAT(tetramino_group[1].x == 1);
+	ASSERT_THAT(tetramino_group[1].y == 1);
+
+	ASSERT_THAT(tetramino_group[2].x == 0);
+	ASSERT_THAT(tetramino_group[2].y == 2);
+
+	ASSERT_THAT(tetramino_group[3].x == 1);
+	ASSERT_THAT(tetramino_group[3].y == 2);
+}
 // TEST(tetramino_group_move_left_blocked)
+TEST(tetramino_group_move_left_blocked)
+{
+	TETRAMINO_BOX_GROUP_SETUP(2, 2);
+	tetramino_group_move_left(tetramino_group, &tetris_map);
+
+	ASSERT_THAT(tetramino_group[0].x == 0);
+	ASSERT_THAT(tetramino_group[0].y == 0);
+
+	ASSERT_THAT(tetramino_group[1].x == 1);
+	ASSERT_THAT(tetramino_group[1].y == 0);
+
+	ASSERT_THAT(tetramino_group[2].x == 0);
+	ASSERT_THAT(tetramino_group[2].y == 1);
+
+	ASSERT_THAT(tetramino_group[3].x == 1);
+	ASSERT_THAT(tetramino_group[3].y == 1);
+}
 // TEST(tetramino_group_move_left_multiple)
 // TEST(tetramino_group_move_right)
+TEST(tetramino_group_move_right)
+{
+	TETRAMINO_BOX_GROUP_SETUP(3, 3);
+	tetramino_group_move_down(tetramino_group, &tetris_map);
+	tetramino_group_move_right(tetramino_group, &tetris_map);
+
+	ASSERT_THAT(tetramino_group[0].x == 1);
+	ASSERT_THAT(tetramino_group[0].y == 1);
+
+	ASSERT_THAT(tetramino_group[1].x == 2);
+	ASSERT_THAT(tetramino_group[1].y == 1);
+
+	ASSERT_THAT(tetramino_group[2].x == 1);
+	ASSERT_THAT(tetramino_group[2].y == 2);
+
+	ASSERT_THAT(tetramino_group[3].x == 2);
+	ASSERT_THAT(tetramino_group[3].y == 2);
+
+}
 // TEST(tetramino_group_move_right_blocked)
 // TEST(tetramino_group_move_right_multiple)
 // TEST(tetramino_group_move_down_wrong_value)
@@ -265,6 +320,9 @@ int main(int argc, char **argv)
 	RUN_TEST(check_row_filled_2x2);
 	RUN_TEST(tetramino_group_init);
 	RUN_TEST(tetramino_group_move_down);
+	RUN_TEST(tetramino_group_move_right);
+	RUN_TEST(tetramino_group_move_left);
+	RUN_TEST(tetramino_group_move_left_blocked);
 	PRINT_TEST_RESULTS();
 	return 0;
 }
